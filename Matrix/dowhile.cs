6fa4217a18cd7b4d27a2786f6ddfbbbcac847056
datbeo1999 <datbeo1999@gmail.com>
2021-03-03 22:43:13 +0700

    yolo

---
+++ Matrix/dowhile.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matrix
+{
+    class dowhile
+    {
+        //static void Main(string[] args)
+        //{
+        //    int row = 0;
+        //    int column = 0;
+        //    int columnloop = 5;
+        //    int rowloop = 10;
+        //    int min = 0;
+        //    int max = 100;
+        //    Random r = new Random();
+        //    do
+        //    {
+        //        row = 0;
+        //        do
+        //        {
+        //            int ran = r.Next(min, max);
+        //            Console.Write("{0,8}", ran);
+        //            row++;
+        //        }
+        //        while (row < rowloop);
+        //        Console.WriteLine();
+        //        column++;
+
+        //    }
+        //    while (column < columnloop);
+        //    Console.ReadKey();
+        //}
+    }
+}
---
+++ Matrix/while.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matrix
+{
+    class @while
+    {
+
+        //static void Main(string[] args)
+        //{
+        //    int ngang = 0;
+        //    int doc = 0;
+        //    int nganglop = 15;
+        //    int doclop = 10;
+        //    int min = -100;
+        //    int max = 100;
+
+        //    Random r = new Random();
+        //    while (doc < doclop)
+        //    {
+        //        ngang = 0;
+        //        while (ngang < nganglop)
+        //        {
+        //            int ra = r.Next(min, max);
+        //            Console.Write("{0,8}", ra);
+        //            ngang++;
+        //        }
+        //        Console.WriteLine();
+        //        doc++;
+
+        //    }
+        //    Console.ReadKey();
+        //}
+    }
+}
